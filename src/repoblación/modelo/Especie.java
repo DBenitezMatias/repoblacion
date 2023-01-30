3c62ba2e700a3eb245fd1be4439626ccf0dc85ea
david <david@david>
2023-01-30 10:56:39 +0100

    Enumerado Especie

---
+++ src/repoblación/modelo/Especie.java
@@ -0,0 +1,11 @@
+package repoblación.modelo;
+
+public enum Especie {
+	ALAMO("Alamo"),ENCINA("Encina"),CASTANO("Castano"),CIPRES("Cipres"),PINO("Pino"),ROBLE("Roble"),OLIVO("Olivo");
+	Especie(String cadenaAmostrar){
+		
+	};
+	
+	
+
+}
